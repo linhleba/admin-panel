852dbbd5f2c8e56c40440be8a48bfe513f534dbe
linhleba <linhleba.it@gmail.com>
2022-02-16 14:52:22 +0700

    update status card dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
-
+import statusCardData from '../assets/JsonData/status-card-data.json';
+import StatusCard from '../components/statuscard/StatusCard';
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h2 className="page-header">Dashboard</h2>
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            {statusCardData.map((object, index) => (
+              <div key={index} className="col-6 card-item">
+                {/* {object.title} */}
+                <StatusCard
+                  icon={object.icon}
+                  count={object.count}
+                  title={object.title}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="col-6">
+          <div className="card full-height">{/* charts */}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
