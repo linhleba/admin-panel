a156737ecde2df8ef3b8f6bdaccd346509a9dbae
linhleba <linhleba.it@gmail.com>
2022-02-18 09:19:22 +0700

    update pagination for table

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -8,14 +8,21 @@ const Table = (props) => {
       ? props.bodyData.slice(0, props.limit)
       : props.bodyData;
 
-  const [dataShow, setDataShow] = useState(sliceData);
+  let [dataShow, setDataShow] = useState(sliceData);
   let maxPage = 1;
   let range = [];
   if (props.limit !== undefined) {
     maxPage = Math.ceil(props.bodyData.length / Number(props.limit));
     range = [...Array(maxPage).keys()];
   }
+  let [currentPage, setCurrentPage] = useState(1);
 
+  const selectPage = (page) => {
+    const start = Number(props.limit) * page;
+    const end = start + Number(props.limit);
+    setDataShow(props.bodyData.slice(start, end));
+    setCurrentPage(page + 1);
+  };
   return (
     <div>
       <div className="table-wrapper">
@@ -31,7 +38,7 @@ const Table = (props) => {
           ) : null}
           {props.bodyData && props.renderBody ? (
             <tbody>
-              {sliceData.map((item, index) => props.renderBody(item, index))}
+              {dataShow.map((item, index) => props.renderBody(item, index))}
             </tbody>
           ) : null}
         </table>
@@ -39,7 +46,13 @@ const Table = (props) => {
       {maxPage > 1 ? (
         <div className="table__pagination">
           {range.map((item, index) => (
-            <div key={index} className="table__pagination-item">
+            <div
+              key={index}
+              className={`table__pagination-item ${
+                currentPage === index + 1 ? 'active' : ''
+              }`}
+              onClick={() => selectPage(index)}
+            >
               {item + 1}
             </div>
           ))}
