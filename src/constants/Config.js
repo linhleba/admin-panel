ff5e379a80d5422704e143399a315bd70f65f73d
18521542 <tranduytrong2000@gmail.com>
2022-03-25 14:53:25 +0700

    format transaction data

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,5 +1,4 @@
-// require('dotenv').config();
-// export const API_URL = 'http://localhost:5432';
+import { custom_host } from "./config.local"
 
-let host = 'http://139.59.246.216';
+let host = custom_host || 'http://139.59.246.216';
 export const API_URL = host;
--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -1,13 +1,16 @@
 import React,  { useEffect, useState } from 'react';
 import OrderTable from '../../components/orders/OrderTable';
 import callAPI from '../../utils/apiCaller';
+import handlePrice from '../../utils/formatPrice/formatPrice';
+import moment from 'moment';
+
 
 function Order(props) {
 
     let [data, setData] = useState([])
 
     let header = [
-        "ID",
+        "Số hóa đơn",
         "Trạng thái giao hàng",
         "Ngày giao hàng",
         "Tổng giá trị",
@@ -17,11 +20,24 @@ function Order(props) {
         "Chi tiết hóa đơn"
     ];
 
+    let formatTransaction = (trans) => {
+        let checkShipdateNull = (trans.ship_date === null);
+        return {
+            ...trans,
+            status: (trans.status == 0) ? "Chưa giao" : "Đã giao",
+            created_at: moment(trans.created_at).format('DD/MM/YYYY'),
+            updated_at: moment(trans.updated_at).format('DD/MM/YYYY'),
+            price_total: handlePrice(trans.price_total),
+            ship_date: (checkShipdateNull) ? "" : moment(trans.ship_date).format('DD/MM/YYYY'),
+        }
+    }
+
     useEffect(() => {
         let getData = async () => {
             const { access_jwt_token } = JSON.parse(localStorage.getItem("profile"));
             let getTransactions = await callAPI("/api/transaction/", "GET", null, {authorization: access_jwt_token})
-            setData(getTransactions.data.message)
+            let data = getTransactions.data.message.map(trans => formatTransaction(trans));
+            setData(data)
         }
         getData()
     }, [])
